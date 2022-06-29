78beacb9ab55fac80214e184dd729af1aec65451
Hugo Delgado <yonkoh925@gmail.com>
2022-06-29 12:45:12 -0700

    intial commit

---
+++ BookContext.cs
@@ -0,0 +1,19 @@
+﻿using MBooks.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MBooks
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Book> book { get; set; }
+    public DbSet<User> user { get; set; }
+
+      
+    }
+}
+    
+
---
+++ Models/BooksModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MBooks.Models
+{
+    public class Book
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string author { get; set; }
+        public string pageLength { get; set; }
+        public bool inCollection { get; set; }
+    }
+
+    public class User
+    {
+        public int id { get; set; }
+
+        //Foreign key relating to book model
+        [ForeignKey("book")]
+        public int bookID { get; set; }
+
+        public Book book { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+}
